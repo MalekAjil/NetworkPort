966c09e16aab432f3635f9dd4f2667f4f3b0589e
Malek Alajil <Aboanasajil@gmail.com>
2023-11-14 01:16:54 +0400

    MK

---
+++ NetworkPort/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace NetworkPort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.desertTheme1 = new Telerik.WinControls.Themes.DesertTheme();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SaveIMGbutton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.MSGtextBox = new System.Windows.Forms.TextBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.ChatRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.SaveIMGbutton);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Location = new System.Drawing.Point(12, 26);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(324, 345);
+            this.panel1.TabIndex = 0;
+            // 
+            // SaveIMGbutton
+            // 
+            this.SaveIMGbutton.BackgroundImage = global::NetworkPort.Properties.Resources.DRIVE___FLOPPY;
+            this.SaveIMGbutton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.SaveIMGbutton.Font = new System.Drawing.Font("Ravie", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SaveIMGbutton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.SaveIMGbutton.Location = new System.Drawing.Point(8, 285);
+            this.SaveIMGbutton.Name = "SaveIMGbutton";
+            this.SaveIMGbutton.Size = new System.Drawing.Size(108, 57);
+            this.SaveIMGbutton.TabIndex = 8;
+            this.SaveIMGbutton.Text = "Save image";
+            this.SaveIMGbutton.UseVisualStyleBackColor = true;
+            this.SaveIMGbutton.Visible = false;
+            this.SaveIMGbutton.Click += new System.EventHandler(this.SaveIMGbutton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Harrington", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 29);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Image";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::NetworkPort.Properties.Resources.BOOTSKINS_ICON;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Font = new System.Drawing.Font("Chiller", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(211, 285);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 57);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Send Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.SendImageButton_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.ImeMode = System.Windows.Forms.ImeMode.Katakana;
+            this.panel3.Location = new System.Drawing.Point(3, 37);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(318, 242);
+            this.panel3.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(312, 236);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.clearToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(104, 70);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Visible = false;
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.panel5);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Location = new System.Drawing.Point(351, 26);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(371, 345);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Harrington", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.OrangeRed;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 29);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Chat";
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = global::NetworkPort.Properties.Resources.NETWORK_CONNECTIONS;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Font = new System.Drawing.Font("Jokerman", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(237, 285);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 57);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Send";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::NetworkPort.Properties.Resources.DOWNLOADS;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Font = new System.Drawing.Font("Algerian", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(3, 285);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 57);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Start Recive";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.ReciveButton_Click);
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Lime;
+            this.panel5.Controls.Add(this.MSGtextBox);
+            this.panel5.ImeMode = System.Windows.Forms.ImeMode.Katakana;
+            this.panel5.Location = new System.Drawing.Point(3, 249);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(341, 27);
+            this.panel5.TabIndex = 3;
+            // 
+            // MSGtextBox
+            // 
+            this.MSGtextBox.Location = new System.Drawing.Point(3, 3);
+            this.MSGtextBox.Name = "MSGtextBox";
+            this.MSGtextBox.Size = new System.Drawing.Size(335, 20);
+            this.MSGtextBox.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Red;
+            this.panel4.Controls.Add(this.ChatRichTextBox);
+            this.panel4.ImeMode = System.Windows.Forms.ImeMode.Katakana;
+            this.panel4.Location = new System.Drawing.Point(3, 37);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(341, 206);
+            this.panel4.TabIndex = 2;
+            // 
+            // ChatRichTextBox
+            // 
+            this.ChatRichTextBox.Location = new System.Drawing.Point(3, 3);
+            this.ChatRichTextBox.Name = "ChatRichTextBox";
+            this.ChatRichTextBox.Size = new System.Drawing.Size(335, 200);
+            this.ChatRichTextBox.TabIndex = 0;
+            this.ChatRichTextBox.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "image | *.jpg";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 383);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.ImeMode = System.Windows.Forms.ImeMode.Katakana;
+            this.Name = "Form1";
+            this.Text = "Chat App";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.Themes.DesertTheme desertTheme1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox MSGtextBox;
+        private System.Windows.Forms.RichTextBox ChatRichTextBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button SaveIMGbutton;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+    }
+}
+
---
+++ NetworkPort/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace NetworkPort
+{
+    public partial class Form1 : Form
+    {
+
+        Socket socketreciver = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        Socket socketsender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+         IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 5020);
+        IPEndPoint remot = new IPEndPoint(IPAddress.Parse("192.168.1.2"), 5020);
+        MemoryStream img_Msg = new MemoryStream();
+      
+        byte[] msg ;// = new byte[1024];
+        byte[] msgReceive ;//= new byte[1024];
+        string buffer = "";
+        int Rec;
+        //
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            MSGtextBox.Select();                             
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyDate)
+        {
+            if (keyDate == Keys.Enter)
+            {
+                SendButton_Click(null, null);
+            }
+            return base.ProcessCmdKey(ref msg, keyDate);
+        }
+
+
+         public void Receive()
+        {
+            ChatRichTextBox.Text += " " + buffer + " : He \n";
+        }
+        public void Send_Image()
+        {
+            try
+            {
+               // IPEndPoint remot = new IPEndPoint(IPAddress.Parse("192.168.1.1"), 5020);
+                socketsender.Connect(remot);
+                string picture = "pictur";
+                msg = new byte[1024];
+                msg = Encoding.ASCII.GetBytes(picture);
+                socketsender.Send(msg);
+                pictureBox1.Image.Save(img_Msg, pictureBox1.Image.RawFormat);
+                msg = new byte[img_Msg.Length];
+                msg = img_Msg.GetBuffer();
+                img_Msg.Close();
+                socketsender.Send(msg);
+                socketsender.Close();
+            }
+            catch (SocketException e)
+            { MessageBox.Show(e.Message); }
+
+        }
+        public void Receive_Image()
+        {
+            try
+            {
+                //socket.Bind(iepany);
+                //socket.Listen(-1);
+                //socket.Accept();
+                
+                MemoryStream new_Image = new MemoryStream(msgReceive);
+                pictureBox1.Image = Image.FromStream(new_Image);
+                SaveIMGbutton.Visible = true;
+                saveToolStripMenuItem.Visible = true;
+            }
+            catch (SocketException err)
+            {
+                MessageBox.Show(err.Message); 
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result != DialogResult.OK)
+                MessageBox.Show("image con't be load because you didn't choose an image");
+            else if (result == DialogResult.OK)
+            {
+                string current_Image = openFileDialog1.FileName;
+                pictureBox1.Image = Image.FromFile(current_Image);
+                SaveIMGbutton.Visible = true;
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveIMGbutton_Click(null, null);
+        }
+
+        private void SendImageButton_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+                openToolStripMenuItem_Click(null, null);
+            Send_Image();
+          
+
+        }
+        private void SaveIMGbutton_Click(object sender, EventArgs e)
+        {
+           
+           if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(saveFileDialog1.FileName);
+            }
+        }
+       
+
+        private void ReciveButton_Click(object sender, EventArgs e)
+        {       
+            EndPoint Remot = (EndPoint)ipep;    
+            try
+            {
+                socketreciver.Bind(ipep);
+                socketreciver.Listen(-1);
+                socketreciver.Accept();
+                while (true)
+                {
+                    msgReceive = new byte[1024];
+                    Rec = socketreciver.Receive(msgReceive);                
+                    buffer = Encoding.ASCII.GetString(msgReceive, 0, Rec);
+                    
+                    if (buffer == "picture")
+                    {
+                        Receive_Image();
+                    }
+                    else
+                    {
+                        Receive();
+                    }
+
+                }
+            }
+            catch (SocketException err)
+            { MessageBox.Show(err.Message); }
+
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+
+            ChatRichTextBox.Text += " Me :    " + MSGtextBox.Text + "\n";
+            Send();
+        }
+        public void Send()
+        {
+            // ارسال رسالة
+            try
+            {
+                socketsender.Connect(remot);
+                msg = new byte[1024];
+                msg = Encoding.ASCII.GetBytes(MSGtextBox.Text);
+                MSGtextBox.Text="";
+                MSGtextBox.Select();
+                socketsender.Send(msg);
+               
+            }
+            catch (SocketException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image=null;
+        }
+
+    }
+}
